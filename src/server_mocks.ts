57b33560efae95a34fa2ec3072a7ac15eebe990c
ad1269 <admohanraj@berkeley.edu>
2022-10-02 23:45:07 -0700

    Added mocked functions

--- src/server_mocks.ts
+++ src/server_mocks.ts
@@ -1,1 +1,12 @@
 // TODO: Stub functions for the Server API calls for local testing
+export function wordleKeyPressed(key: string) {
+
+}
+
+export function checkGuess() {
+
+}
+
+export function deleteKeyPressed() {
+
+}
